59cf152dcad4b640d7b39faef5ca9695601154ba
AbrorRustamov <abrorrustamov446@gmail.com>
2023-09-04 05:54:27 +0500

    abror dev

--- js/main.js
+++ js/main.js
@@ -1,8 +1,27 @@
 var elMovLis = document.querySelector('.movie__list')
+var elSelYear = document.querySelector('.sel__year')
+var elSelRanting = document.querySelector('.sel__ranting')
+var elSelCategory = document.querySelector('.sel__category')
+var elInp = document.querySelector('.inp_search')
 
-var partMovies = movies.slice(0, 20)
+
+var partMovies = movies.slice(0, 100)
+var arrCategory = []
 partMovies.forEach((item) => {
-    console.log(item.Title);
+  if (arrCategory.includes(item.Categories) == false) {
+    arrCategory.push(item.Categories)
+  }
+})
+arrCategory.forEach((item) => {
+  var newOptipn = document.createElement('option')
+  newOptipn.textContent = item
+  newOptipn.value = item
+  elSelCategory.appendChild(newOptipn)
+})
+
+function addItem(data) {
+  elMovLis.innerHTML = ''
+  partMovies.forEach((item) => {
     var newLi = document.createElement('li')
     newLi.innerHTML = `
     <div class="card" style="width: 18rem;">
@@ -16,4 +35,44 @@ partMovies.forEach((item) => {
   </div>
 </div>`
     elMovLis.appendChild(newLi)
+  })
+}
+addItem(partMovies)
+
+elSelYear.addEventListener('change', () => {
+  elMovLis.innerHTML = ''
+  if (elSelYear.value == 'old') {
+    addItem(partMovies.sort((a, b) => a.movie_year - b.movie_year));
+
+  } else if (elSelYear.value == 'new') {
+    addItem(partMovies.sort((a, b) => b.movie_year - a.movie_year));
+  }
+  else {
+    addItem(partMovies);
+  }
 })
+elSelRanting.addEventListener('change', () => {
+  elMovLis.innerHTML = ''
+  if (elSelRanting.value == 'min') {
+    addItem(partMovies.sort((a, b) => a.imdb_rating - b.imdb_rating));
+
+  } else if (elSelRanting.value == 'max') {
+    addItem(partMovies.sort((a, b) => b.imdb_rating - a.imdb_rating));
+  }
+  else {
+    addItem(partMovies);
+  }
+})
+
+elSelCategory.addEventListener('change', () => {
+  if (elSelCategory.value == '') {
+    addItem(partMovies);
+  }
+  else {
+    addItem(partMovies.filter((item) => item.Categories == elSelCategory.value));
+  }
+})
+function fnSearch() {
+  addItem(partMovies.filter((item) => item.Title.toString().toLowerCase().includes(elInp.value.trim().toLowerCase()) == true));
+
+}
